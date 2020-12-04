5f40f160f633721934e2428e1a93176c40cf4f4f
stefanbanu <stefanbanu1@gmail.com>
2020-12-04 05:08:39 +0200

    moved the data to the backend and started 2 npm projects from backend/express and frontend.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
+console.log('inside server.js');
 const express = require('express');
+const cors = require('cors');
 const data = require('./data.js');
 const app = express();
+app.use(cors());
 
-app.get('/api/products', (request, response) => {
-    response.send(data.products);
+app.get('/api/products', (req, res) => {
+    console.log('retrieving products from server.js');
+    res.send(data.products);
 });
 
 app.listen(5000, () => {
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -8,14 +8,14 @@ const routes = {
     '/product/:id': ProductScreen
 }
 
-const router = () => {
+const router = async () => {
     const request = parseRequestUrl();
     const parseUrl = (request.resource ? `/${request.resource}` : '/') +
                      (request.id ? '/:id': '') +
                      (request.verb ? `/${request.verb}` : '');
     const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
     const main = document.getElementById('main-container');
-    main.innerHTML = screen.render();
+    main.innerHTML = await screen.render();
 };
 
 window.addEventListener('load', router);
