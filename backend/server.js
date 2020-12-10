841ebbb2d343b2013d48132b173da0d12ae29466
stefanbanu <stefanbanu1@gmail.com>
2020-12-10 07:23:39 +0200

    fixed displaying half star.

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,10 @@ import data from './data';
 
 const app = express();
 app.use(cors());
-
 app.get('/api/products', (req, res) => {
-    console.log('retrieving products from server.js');
-    res.send(data.products);
+  res.send(data.products);
 });
 
 app.listen(5000, () => {
-    console.log('serve at http://localhost:5000');
+  console.log('serve at http://localhost:5000');
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,22 @@
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import { parseRequestUrl } from "./utils";
-import Error404Screen from "./screens/Error404Screen";
+import HomeScreen from './srceens/HomeScreen';
+import ProductScreen from './srceens/ProductScreen';
+import { parseRequestUrl } from './utils';
+import Error404Screen from './srceens/Error404Screen';
 
 const routes = {
-  "/": HomeScreen,
-  "/product/:id": ProductScreen,
+  '/': HomeScreen,
+  '/product/:id': ProductScreen,
 };
-
 const router = async () => {
   const request = parseRequestUrl();
   const parseUrl =
-    (request.resource ? `/${request.resource}` : "/") +
-    (request.id ? "/:id" : "") +
-    (request.verb ? `/${request.verb}` : "");
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? '/:id' : '') +
+    (request.verb ? `/${request.verb}` : '');
   const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
-  const main = document.getElementById("main-container");
+
+  const main = document.getElementById('main-container');
   main.innerHTML = await screen.render();
 };
-
-window.addEventListener("load", router);
-
-window.addEventListener("hashchange", router);
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
